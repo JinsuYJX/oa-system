7b9d813b1c2b3436b05f19a4ad4dce896d06d189
azwcl <azwcl@outlook.com>
2022-09-04 22:03:05 +0800

    [init] init

---
+++ oa-back-end/oa-provider/src/main/java/com/azwcl/oa/provider/OaApplication.java
@@ -0,0 +1,19 @@
+package com.azwcl.oa.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * oa 系统 application
+ *
+ * @author az
+ * @date 2022/09/04
+ */
+@SpringBootApplication
+public class OaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OaApplication.class, args);
+	}
+
+}
---
+++ oa-back-end/oa-provider/src/test/java/com/azwcl/oa/provider/OaProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azwcl.oa.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OaProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
