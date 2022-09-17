452eda8d05b44ed1d78c787bcc22de9911e635c5
azwcl <azwcl@outlook.com>
2022-09-17 20:38:29 +0800

    OSS 文件服务

---
+++ oa-back-end/src/main/java/com/azwcl/oa/infrastructure/common/enums/FileSystemModel.java
@@ -0,0 +1,53 @@
+package com.azwcl.oa.infrastructure.common.enums;
+
+import lombok.Getter;
+
+import java.util.Objects;
+
+/**
+ * todo
+ *
+ * @author az
+ * @date 2022/09/17
+ */
+@Getter
+public enum FileSystemModel {
+
+    /**
+     * 未知
+     */
+    NONE(-1),
+
+    /**
+     * 本地文件存储
+     */
+    LOCAL(0),
+
+    /**
+     * 阿里云 OSS 文件存储
+     */
+    ALIYUN_OSS(1);
+
+    private final int model;
+
+    FileSystemModel(int model) {
+        this.model = model;
+    }
+
+    /**
+     * 枚举 of 方法
+     *
+     * @param model 需要判断的 model
+     * @return 返回匹配枚举
+     */
+    public static FileSystemModel of(int model) {
+        FileSystemModel[] values = values();
+
+        for (FileSystemModel value : values) {
+            if (Objects.equals(value.getModel(), model)) {
+                return value;
+            }
+        }
+        return NONE;
+    }
+}
