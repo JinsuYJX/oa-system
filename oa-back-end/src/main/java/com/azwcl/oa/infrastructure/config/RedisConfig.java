2e6d06e735ab19a518174d0340561c8093b195ab
azwcl <azwcl@outlook.com>
2022-09-21 07:44:26 +0800

    调整 redis 序列化器，去除 @Class ；减少内存开销

--- oa-back-end/src/main/java/com/azwcl/oa/infrastructure/config/RedisConfig.java
+++ oa-back-end/src/main/java/com/azwcl/oa/infrastructure/config/RedisConfig.java
@@ -1,10 +1,13 @@
 package com.azwcl.oa.infrastructure.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * redis 配置
@@ -14,7 +17,9 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
  */
 
 @Configuration
+@RequiredArgsConstructor
 public class RedisConfig {
+    private final ObjectMapper objectMapper;
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
@@ -22,13 +27,14 @@ public class RedisConfig {
 
         template.setConnectionFactory(factory);
 
-        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer();
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
 
         // 使用 jackson 序列化反序列化 key value
-        template.setKeySerializer(serializer);
+        template.setKeySerializer(stringRedisSerializer);
         template.setValueSerializer(serializer);
         // 使用 jackson 序列化反序列化 hash key value
-        template.setHashKeySerializer(serializer);
+        template.setHashKeySerializer(stringRedisSerializer);
         template.setHashValueSerializer(serializer);
 
         template.afterPropertiesSet();
