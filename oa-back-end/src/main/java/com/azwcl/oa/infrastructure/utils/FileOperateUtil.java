9bc36487be0ece8f9312a49992911b521ab6c737
zero <debuggerzero@gmail.com>
2022-09-21 20:22:05 +0800

    本地文件读取工具类

--- oa-back-end/src/main/java/com/azwcl/oa/infrastructure/utils/FileOperateUtil.java
+++ oa-back-end/src/main/java/com/azwcl/oa/infrastructure/utils/FileOperateUtil.java
@@ -3,6 +3,7 @@ package com.azwcl.oa.infrastructure.utils;
 import com.azwcl.oa.infrastructure.common.file.FileOperate;
 
 import java.io.*;
+import java.nio.file.Files;
 
 /**
  * 本地文件读取工具类
@@ -12,74 +13,48 @@ import java.io.*;
  */
 public class FileOperateUtil implements FileOperate {
 
-    private Boolean isPathExist(String path) {
-        File filePath = new File(path);
-        return filePath.exists() && filePath.isDirectory();
-    }
-
     @Override
     public Boolean isExist(String path, String filename) {
-        return isPathExist(path) && (new File(path, filename)).exists();
+        return new File(path, filename).exists();
     }
 
     @Override
     public InputStream getFile(String path, String filename) throws IOException {
 
-        if (Boolean.TRUE.equals(isExist(path, filename))) {
-            return new FileInputStream(new File(path, filename));
+        File file = new File(path, filename);
+
+        if (!file.exists()) {
+            throw new IOException();
         }
 
-        if (!(new File(path).isDirectory())) {
-            throw new IOException("This is not the correct directory format");
-        }
-
-        //判断目录是否存在，若不存在则先创建目录
-        File filePath = new File(path);
-        if (!filePath.exists()) {
-            boolean mkdir = filePath.mkdir();
-            if (!mkdir) {
-                throw new IOException("File path creation failure...");
-            }
-        }
-        File file = new File(filePath, filename);
-        boolean newFile = file.createNewFile();
-        if (newFile) {
-            return new FileInputStream(file);
-        } else {
-            throw new IOException("File path creation failure...");
-        }
+        return Files.newInputStream(file.toPath());
     }
 
     @Override
     public void save(String path, String filename, InputStream stream) throws IOException {
-        if (Boolean.FALSE.equals(isPathExist(path))) {
-            File filePath = new File(path);
-            boolean mkdir = filePath.mkdir();
-            if (!mkdir) {
-                throw new IOException("File path creation failure...");
-            }
+        File file = new File(path, filename);
+
+        if (!file.getParentFile().exists() && !file.getParentFile().mkdirs()) {
+            throw new IOException();
         }
-        try (OutputStream out = new FileOutputStream(new File(path, filename))) {
-            int len;
-            byte[] buffer = new byte[1024];
-            while ((len = stream.read(buffer)) != -1) {
-                out.write(buffer, 0, len);
-            }
-        } catch (FileNotFoundException e) {
-            throw new IOException(e);
+
+        OutputStream out = Files.newOutputStream(file.toPath());
+
+        int len;
+        byte[] buffer = new byte[1024];
+        while ((len = stream.read(buffer)) != -1) {
+            out.write(buffer, 0, len);
         }
+
+        out.close();
     }
 
     @Override
     public void delete(String path, String filename) throws IOException {
-        if (Boolean.FALSE.equals(isExist(path, filename))) {
-            return;
-        }
-
         File file = new File(path, filename);
-        if (!file.delete()) {
-            throw new IOException("File deletion failure...");
+
+        if (!file.exists() || !file.delete()) {
+            throw new IOException();
         }
     }
 }
-
